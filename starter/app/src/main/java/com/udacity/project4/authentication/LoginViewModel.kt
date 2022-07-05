714795a982da0a20edec7f7e59dba6af4e54a039
ducnt2010 <mrduca2ltt@gmail.com>
2022-07-05 08:04:28 +0700

    enable login

---
+++ starter/app/src/main/java/com/udacity/project4/authentication/LoginViewModel.kt
@@ -0,0 +1,19 @@
+package com.udacity.project4.authentication
+
+import androidx.lifecycle.map
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel : ViewModel() {
+
+    enum class AuthenticationState {
+        AUTHENTICATED, UNAUTHENTICATED, INVALID_AUTHENTICATION
+    }
+
+    val authenticationState = FirebaseUserLiveData().map { user ->
+        if (user != null) {
+            AuthenticationState.AUTHENTICATED
+        } else {
+            AuthenticationState.UNAUTHENTICATED
+        }
+    }
+}
